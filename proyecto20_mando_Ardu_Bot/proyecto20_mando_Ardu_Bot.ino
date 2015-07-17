cba527e1b5371550b6bf6d2ed50ff5699916577e
natiexperiencia <natiexperiencia@gmail.com>
2015-07-17 21:45:22 +0200

    sumar sensor

--- proyecto20_mando_Ardu_Bot/proyecto20_mando_Ardu_Bot.ino
+++ proyecto20_mando_Ardu_Bot/proyecto20_mando_Ardu_Bot.ino
@@ -6,7 +6,7 @@
 SoftwareSerial blue(2,3);//tx,rx
 Ultrasonic ultrasonic(5,4);//trig,echo,frecuencia
 
-//constantes (IN1,IN2 & ENA = motores I -- IN3,IN4,ENB = motores D
+//constantes (IN1,IN2 & ENA = motores D -- IN3,IN4,ENB = motores I
 const int controlLed = A0;
 const int frontLed = A2;
 const int IN1 = 6;
@@ -15,10 +15,11 @@ const int ENA = 11;
 const int IN3 = 8;
 const int IN4 = 9;
 const int ENB = 10;
-const int sensor = 12;
+const int sensor = A5;
 
 //variables
-int distancia;
+int distanciaD;
+int distanciaB;
 char orden;
 int potencia = 255;
 int obstaculo;
@@ -42,9 +43,13 @@ void setup() {
 }
 
 void loop() {
-  distancia = ultrasonic.Ranging(CM);
-  Serial.println(digitalRead(sensor));
-  if(!comprobar()){
+  distanciaD = ultrasonic.Ranging(CM);
+  distanciaB = analogRead(sensor);
+  Serial.println(distanciaD);
+  if(!comprobarDelante()){ 
+    stopM();
+  }
+  if(!comprobarDetras()){
     stopM();
   }
 
@@ -52,7 +57,7 @@ void loop() {
     orden = blue.read();
     switch(orden){
       case 'a':
-        if(comprobar()){
+        if(comprobarDelante()){
           adelante(potencia);
           digitalWrite(controlLed,HIGH);
           blue.println("Adelante");
@@ -62,7 +67,7 @@ void loop() {
        break;
 
       case 'b':
-        if(comprobar()){
+        if(comprobarDetras()){
           atras(potencia);
           digitalWrite(controlLed,HIGH);
           blue.println("Atrás");
@@ -111,19 +116,19 @@ void atras(int potencia){
 }
 
 void derecha(int potencia){
-    digitalWrite(IN1,HIGH);
-    digitalWrite(IN2,LOW);
-    digitalWrite(IN3,LOW);
-    digitalWrite(IN4,HIGH);
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,HIGH);
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,LOW);
     analogWrite(ENA,potencia);
     analogWrite(ENB,potencia);
 }
 
 void izquierda(int potencia){
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,HIGH);
-    digitalWrite(IN3,HIGH);
-    digitalWrite(IN4,LOW);
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,HIGH);
     analogWrite(ENA,potencia);
     analogWrite(ENB,potencia);
 }
@@ -133,11 +138,19 @@ void stopM(){
     analogWrite(ENB,0);
 }
 
-boolean comprobar(){
-  if(distancia > 15){
+boolean comprobarDelante(){
+  if(distanciaD > 20){
         return true;
       }else{
         return false;
       }
+}
+
+boolean comprobarDetras(){
+  if(distanciaB > 50){
+    return true;
+  }else{
+    return false;
+  }
 }
 
